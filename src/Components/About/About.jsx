04f3f9b1a301d5b7072c654976fdc77ac6b995da
qoziboyeva-munisa <bouqarovxamid@gmail.com>
2024-05-13 18:46:18 +0500

    api

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -9,93 +9,76 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 
-function About() { 
- const url = 'https://autoapi.dezinfeksiyatashkent.uz/api/cities'  
-//  const img ='23c1b9b1-81fa-4392-a63f-111e8f1b56a1.jpeg'
-const [data, setData]=useState([])
-const getAbout=()=>{
-    
-    axios({
-        // imgage_src :img,
-        url:url,
-        method:"GET",
-    }).then((res)=>{
-         setData(res.data.data)
-    }).catch((err)=>{
-        console.log(err);
-    })
-}
-    
-    useEffect(()=>{
+function About() {
+    const url = 'https://autoapi.dezinfeksiyatashkent.uz/api/cities'
+    const urlimage = 'https://autoapi.dezinfeksiyatashkent.uz/api/uploads/images/'
+    const [data, setData] = useState([])
+    const getAbout = () => {
+
+        axios({
+            url: url,
+            method: "GET",
+        }).then((res) => {
+            setData(res.data.data)
+        }).catch((err) => {
+            console.log(err);
+        })
+    }
+
+    useEffect(() => {
         getAbout()
-    },[])
-  
-    return(
-    
-  <>
-   <div className="about" id='about'>
-    <div className="container about__container">
-     
-        <h1 className="about__title">About Us</h1>
-        <p className="about__text">Sagittis nibh scelerisque vitae eget vulputate sem elementum sed neque nisi felis non ultrices massa id egestas quam velit pretium nu.</p>
-        <ul className="about__list">
-            <li className="about__item">
-                <img className='about__img' src={img1} alt="" />
-                <div className="about__text__name">
-                {
-            data && data.map((item,index)=>(
-                <div key={index}>
-                    <p className="">{item.name}</p> 
-                   <p className="">{item.text}</p>
-                   <p className="">{item.img}</p>
-                    
+    }, [])
 
+     return (
+
+        <>
+            <div className="about" id='about'>
+                <div className="container about__container">
+
+                    <h1 className="about__title">About Us</h1>
+                    <p className="about__text">Sagittis nibh scelerisque vitae eget vulputate sem elementum sed neque nisi felis non ultrices massa id egestas quam velit pretium nu.</p>
+                    <ul className="about__list">
+                        <li className="about__item">
+                      
+                            <div className="about__text__name">
+
+                                <div  >
+                                    <img style={{width:'100%'}} src={`${urlimage}${data[0]?.image_src}`} alt="" />
+                                    <p className="">{data[0]?.name}</p>
+                                    <p className="">{data[0]?.text}</p>
+                                    <p className="">{data[0]?.img}</p>
+                                </div>
+
+
+                            </div>
+                        </li>
+                        <li className="about__item">
+                            
+                            <div className="about__text__name">
+                                <div  >
+                                    <img style={{width:'100%'}} src={`${urlimage}${data[1]?.image_src}`} alt="" />
+                                    <p className="">{data[1]?.name}</p>
+                                    <p className="">{data[1]?.text}</p>
+                                    <p className="">{data[1]?.img}</p>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="about__item">
+                            
+                            <div className="about__text__name">
+                                <div  >
+                                    <img style={{width:'100%'}} src={`${urlimage}${data[2]?.image_src}`} alt="" />
+                                    <p className="">{data[2]?.name}</p>
+                                    <p className="">{data[2]?.text}</p>
+                                    <p className="">{data[2]?.img}</p>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
-            ))
-
-        }
-
-                </div>
-            </li>
-            <li className="about__item">
-                <img className='about__img' src={img2} alt="" />
-                <div className="about__text__name">
-                {
-            data && data.map((item,index)=>(
-                <div key={index}>
-                   <p className="">{item.name}</p> 
-                   <p className="">{item.text}</p>
-                   <p className="">{item.img}</p>
-                    
-
-                </div>
-            ))
-
-        }   
-                </div>
-            </li>
-            <li className="about__item">
-                <img className='about__img' src={img3} alt="" />
-                <div className="about__text__name">
-                {
-            data && data.map((item,index)=>(
-                <div key={index}>
-                   <p className="">{item.name}</p> 
-                   <p className="">{item.text}</p>
-                   <p className="">{item.img}</p>
-                    
-
-                </div>
-            ))
-
-        }  
-                </div>
-            </li>
-        </ul>
-    </div>
-   </div>
-    </>
-)
+            </div>
+        </>
+    )
 }
 
 export default About
